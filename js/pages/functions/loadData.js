ada18d3917670f96dc93fa4644d16bee333933e5
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-09-06 00:35:11 +0700

    feat: Thêm tính năng hiển thị 0 bản ghi

--- js/pages/functions/loadData.js
+++ js/pages/functions/loadData.js
@@ -33,9 +33,12 @@ import tdCheckbox from "./tdCheckbox.js"
 
         // Gọi API từ server để lấy dữ liệu
         fetch(apiFetch, {method: "GET"})
-            .then(res => res.json())
             .then(res => {
-                console.log(res)
+               if(res.status == 200){
+                   return res.json()
+               }
+            })
+            .then(res => {
                 // Xóa dữ liệu cũ trong table
                 let table = $("#table__employee .table__body--real")
                 table.empty()
@@ -46,63 +49,70 @@ import tdCheckbox from "./tdCheckbox.js"
                 let ths = $("#table__employee thead th")
                 // Xử lý dữ liệu
                 let count = 1
-                for(const emp of res['Data']){
-                    // tạo cột tr
-                    let trHTML = $(`<tr></tr>`);
-                    // tạo td checkbox có từng id riêng
-                    let tdCB = tdCheckbox(count)
-                    trHTML.append(tdCB)
-                    for(let th = 1; th< ths.length -1; th++){
-                        // lấy ra thông tin propName
-                        const propName = $(ths[th]).attr("propName")
-                        let value = emp[propName]
-                        let td = ""
-                        // format văn bản
-                        const formatDate = ths[th].hasAttribute("format-date");
-                        const formatMoney = ths[th].hasAttribute("format-money");
-                        const formatGender = ths[th].hasAttribute("format-gender");
-                        if (formatDate){
-                            value = common.formatDate(value);
-                            td = `<td  class="text__align--center">${value|| ''}</td>`
+
+                // nếu không có bản ghi nào thì không render ra td nào và số bản ghi hiển thị bằng 0
+                if(res != undefined){
+                    for(const emp of res['Data']){
+                        // tạo cột tr
+                        let trHTML = $(`<tr></tr>`);
+                        // tạo td checkbox có từng id riêng
+                        let tdCB = tdCheckbox(count)
+                        trHTML.append(tdCB)
+                        for(let th = 1; th< ths.length -1; th++){
+                            // lấy ra thông tin propName
+                            const propName = $(ths[th]).attr("propName")
+                            let value = emp[propName]
+                            let td = ""
+                            // format văn bản
+                            const formatDate = ths[th].hasAttribute("format-date");
+                            const formatMoney = ths[th].hasAttribute("format-money");
+                            const formatGender = ths[th].hasAttribute("format-gender");
+                            if (formatDate){
+                                value = common.formatDate(value);
+                                td = `<td  class="text__align--center">${value|| ''}</td>`
+                            }
+                            else
+                            if (formatMoney){
+                                value = common.formatMoneyVND(value);
+                                td = `<td  class="text__align--right">${value|| ''}</td>`
+                            }
+                            if (formatGender){
+                                value = common.formatGender(value)
+                                td = `<td  class="text__align--left">${value|| ''}</td>`
+                            }
+                            else{
+                                // tạo cột td
+                                td = `<td>${value|| ''}</td>`
+                            }
+                            trHTML.append(td)
                         }
-                        else
-                        if (formatMoney){
-                            value = common.formatMoneyVND(value);
-                            td = `<td  class="text__align--right">${value|| ''}</td>`
-                        }
-                        if (formatGender){
-                            value = common.formatGender(value)
-                            td = `<td  class="text__align--left">${value|| ''}</td>`
-                        }
-                        else{
-                            // tạo cột td
-                            td = `<td>${value|| ''}</td>`
-                        }
-                        trHTML.append(td)
-                    }
-                    // tạo td chức năng
-                    let tdEdit = `<td class="text__align--center">    
-                                    <div class="contextmenu">
-                                        <div class="contextmenu__main">
-                                            <div class="contextmenu__button">
-                                                Sửa
-                                            </div>
-                                            <div tabindex="0" class="contextmenu__dropicon">
-                                                <div class="contextmenu__menu">
-                                                    <div tabindex="0" class="contextmenu__item">Nhân bản</div>
-                                                    <div tabindex="0" class="contextmenu__item  contextmenu__deletebtn" valueId='${emp["EmployeeId"]}' valueName='${emp["FullName"]}'>Xóa</div>
-                                                    <div tabindex="0" class="contextmenu__item">Ngưng sử dụng</div>
+                        // tạo td chức năng
+                        let tdEdit = `<td class="text__align--center">    
+                                        <div class="contextmenu">
+                                            <div class="contextmenu__main">
+                                                <div class="contextmenu__button">
+                                                    Sửa
+                                                </div>
+                                                <div tabindex="0" class="contextmenu__dropicon">
+                                                    <div class="contextmenu__menu">
+                                                        <div tabindex="0" class="contextmenu__item">Nhân bản</div>
+                                                        <div tabindex="0" class="contextmenu__item  contextmenu__deletebtn" valueId='${emp["EmployeeId"]}' valueName='${emp["FullName"]}'>Xóa</div>
+                                                        <div tabindex="0" class="contextmenu__item">Ngưng sử dụng</div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </td>`
-                    trHTML.append(tdEdit)
-                    table.append(trHTML)
-                    count++
+                                    </td>`
+                        trHTML.append(tdEdit)
+                        table.append(trHTML)
+                        count++
+                    }
+                    // thêm tổng số records trong bản ghi vào trong page navigation
+                    $(".page__navi .page__records").text(res["TotalRecord"])
+                }else{
+                    // nếu không có bản ghi nào thì không render ra td nào và số bản ghi hiển thị bằng 0
+                    $(".page__navi .page__records").text("0")
                 }
-                // thêm tổng số records trong bản ghi vào trong page navigation
-                $(".page__navi .page__records").text(res["TotalRecord"])
                 // ẩn loading đi
                 $(loading).addClass(MISAEnum.table.HIDE)
             })
