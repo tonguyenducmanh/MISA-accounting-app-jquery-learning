8a8759761604b730e2a4246f723b81648a5adb50
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-09-06 19:45:10 +0700

    feat: Thêm người dùng (chưa validate)

--- js/pages/employee.js
+++ js/pages/employee.js
@@ -63,6 +63,8 @@ $(document).ready(function(){
     $(document).on("click", "#form .form__cancel", handleForm.cancelForm)
     // khi ấn esc thì cũng hiện popup như click vào hủy
     $(document).on("keydown",{event_type: "cancelForm"}, handleKeyDown)
+    // ấn vào nút cất thì tiến hành lưu
+    $(document).on("click", "#form .form__save--close", handleForm.saveNew)
 
     
     // handle popup ask
