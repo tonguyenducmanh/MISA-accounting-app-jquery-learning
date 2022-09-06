dd7141d53f3767ccbd82bae91258941680e41240
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-09-06 20:06:30 +0700

    feat: Miscellaneous

--- js/resource.js
+++ js/resource.js
@@ -1,5 +1,10 @@
 var MISAResource = {
-
+    ErrorValidate: {
+        EmployeeCodeNotEmpty:{
+            VI: "Mã nhân viên không được phép để trống",
+            EN: "Employee code is not empty"
+        },
+    },
 }
 
 export default MISAResource
